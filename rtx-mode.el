3b0e0eedef8098d6bf62ccc44f531ef88d9fbd86
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2023-01-01 10:36:49 +0100

    Switch to builtin emacs treesit

--- rtx-mode.el
+++ rtx-mode.el
@@ -1,10 +1,10 @@
 ;;; rtx-mode.el --- major mode for editing RTX files
 
-;; Copyright (C) 2021 Kevin Brubeck Unhammer
+;; Copyright (C) 2021-2023 Kevin Brubeck Unhammer
 
 ;; Author: Kevin Brubeck Unhammer <unhammer@fsfe.org>
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1") (tree-sitter-langs "0.10.7"))
+;; Package-Requires: ((emacs "29.0"))
 ;; Url: http://wiki.apertium.org/wiki/Emacs
 ;; Keywords: languages
 
@@ -31,15 +31,14 @@
 ;;
 ;; (use-package rtx-mode
 ;;   :defer t
-;;   :mode (("\\.rtx\\'" . rtx-mode))
-;;   :config
-;;   (add-hook 'rtx-mode-hook #'tree-sitter-mode))
+;;   :mode (("\\.rtx\\'" . rtx-mode)))
 
 ;;; Code:
 
 (defconst rtx-mode-version "0.1.0" "Version of rtx-mode.")
 
 (require 'xref)
+(require 'treesit)
 
 ;;;============================================================================
 ;;;
@@ -59,6 +58,81 @@
     table)
   "Syntax table for RTX mode.")
 
+(defcustom rtx-indent-offset 4
+  "Basic size of one indentation step."
+  :type 'integer
+  :safe 'integerp)
+
+(defvar rtx--treesit-indent-rules
+  '((rtx
+     ((parent-is "reduce_rule_group") parent-bol rtx-indent-offset)
+     ((parent-is "reduce_rule") parent-bol rtx-indent-offset)
+     ((parent-is "attr_rule") parent-bol rtx-indent-offset)))
+  "Tree-sitter indentation rules for `rtx-mode'.")
+
+(defun rtx--treesit-defun-name (node)
+  "Return the defun name of NODE.
+Return nil if there is no name or if NODE is not a defun node."
+  (pcase (treesit-node-type node)
+    ("attr_rule" (treesit-node-text
+                  (treesit-node-child node 0) t))
+    ("reduce_rule_group" (treesit-node-text
+                          (treesit-node-child node 0) t))))
+
+(defvar rtx--treesit-settings
+  (treesit-font-lock-rules
+   :feature 'comment
+   :language 'rtx
+   '((comment) @font-lock-comment-face)
+
+   :feature 'string
+   :language 'rtx
+   '((string) @font-lock-string-face)
+
+   :feature 'keyword
+   :language 'rtx
+   '([(if_tok)
+      (elif_tok)
+      (else_tok)
+      (always_tok)] @font-lock-keyword-face)
+
+   :feature 'variable
+   :language 'rtx
+   '((ident) @font-lock-variable-name-face)
+
+   ;; :language 'rtx
+   ;; :feature 'operator
+   ;; `([(str_op) "=" "~=" "^=" "|=" "*=" "$="] @font-lock-operator-face)
+
+   ;; TODO: these are not matched:
+   :feature 'function
+   :language 'rtx
+   '((reduce_rule_group (ident)) @font-lock-function-name-face
+     (attr_rule :name (ident)) @font-lock-function-name-face)
+
+   :feature 'function
+   :language 'rtx
+   '((ident) @font-lock-function-name-face)
+
+   ;; TODO: these are not matched
+   :feature 'constant
+   :language 'rtx
+   '((reduce_rule (string)) @font-lock-constant-face
+     (blank) @font-lock-constant-face)
+
+   :feature 'weight
+   :language 'rtx
+   '((weight) @font-lock-property-face)
+
+   :feature 'bracket
+   :language 'rtx
+   '((["(" ")" "[" "]" "{" "}"]) @font-lock-bracket-face)
+
+   :feature 'error
+   :language 'rtx
+   '((ERROR) @error))
+  "Tree-sitter font-lock settings for `rtx-mode'.")
+
 ;;;###autoload
 (define-derived-mode rtx-mode prog-mode "RTX"
   "Major mode for editing apertium-recursive .rtx-files.
@@ -66,7 +140,38 @@ RTX-mode provides the following specific keyboard key bindings:
 
 \\{rtx-mode-map}"
   (set (make-local-variable 'comment-start) "!")
-  (set (make-local-variable 'comment-start-skip) "!+[\t ]*"))
+  (set (make-local-variable 'comment-start-skip) "!+[\t ]*")
+
+  (if (not (treesit-ready-p 'rtx))
+      (message "Run `M-x rtx-install-tree-sitter' to install syntax highlighting, indentation and imenu support.")
+    ;; Tree-sitter specific setup.
+    (treesit-parser-create 'rtx)
+    (setq-local treesit-simple-indent-rules rtx--treesit-indent-rules)
+    (setq-local treesit-defun-type-regexp "reduce_rule_group")
+    (setq-local treesit-defun-name-function #'rtx--treesit-defun-name)
+    (setq-local treesit-font-lock-settings rtx--treesit-settings)
+    (setq-local treesit-font-lock-feature-list
+                '((selector comment query keyword function)
+                  (property constant string weight)
+                  (error variable function operator bracket)))
+    (setq-local treesit-simple-imenu-settings
+                `(( "Reduction" ,(rx bos "reduce_rule_group" eos)
+                    nil nil)
+                  ( "Attribute" ,(rx bos "attr_rule" eos)
+                    nil nil)))
+    (treesit-major-mode-setup)))
+
+(defun rtx-install-tree-sitter ()
+  "Install the tree-sitter grammar for rtx."
+  (interactive)
+  (let ((treesit-language-source-alist
+         '((rtx "https://github.com/unhammer/tree-sitter-apertium" "tree-sitter-rtx/src" "tree-sitter-rtx"))))
+    (treesit-install-language-grammar 'rtx))
+  ;; refresh currently open rtx buffers:
+  (mapc (lambda (b) (with-current-buffer b
+                 (when (eq major-mode 'rtx-mode)
+                   (rtx-mode))))
+        (buffer-list)))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.rtx$" . rtx-mode))
